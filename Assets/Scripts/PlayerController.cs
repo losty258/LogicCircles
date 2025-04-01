734b1d197ed7593d38e8bd1c98d3dd00b3842b02
losty258 <dchen000@email.cpcc.edu>
2025-04-01 15:52:24 -0400

    Added Player Controller and able to rotate with the nodes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    [SerializeField] private bool right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!right)
+        {
+            transform.Rotate(Vector3.back * 180);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(Vector3.right * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DirectionalNode dn = other.GetComponent<DirectionalNode>();
+        Debug.Log(dn.current);
+
+        // Check direction 
+        float rot = 0;
+        Vector3 dir = Vector3.zero;
+
+        if (dn.current.Equals("right"))
+        {
+            dir = Vector3.back;
+            rot = 90;
+        }
+        else if (dn.current.Equals("left"))
+        {
+            dir = Vector3.forward;
+            rot = 90;
+        }
+
+        transform.Rotate(dir * rot); //back is right, forward is left
+    }
+}
